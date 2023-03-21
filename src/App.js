0c114a7cdc69870393bfba56813237f9228723fe
1sarthakbhardwaj <7sarthakbhardwaj@gmail.com>
2023-03-22 01:06:18 +0530

    Dynamic value in templates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 function App() {
+  // Only number string are fine, not for boolean and objects
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>You got {likes} likes</p>
+        <p>random number is {Math.random() * 100}</p>
       </div>
     </div>
   );
