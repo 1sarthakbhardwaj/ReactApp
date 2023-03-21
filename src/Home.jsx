ec81a88c1497e66f603b1c43036d6cbec88f8b71
1sarthakbhardwaj <7sarthakbhardwaj@gmail.com>
2023-03-22 02:20:34 +0530

    useState Hooks

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
+
 const Home = () => {
+    // let name = 'sujan';
+   const[name, setName] = useState('Suja');
+   const[age, setAge] = useState(18);
 
     const handleClick = () => {
-        console.log('clicked');
+       setName('Suju bhai')
+       setAge(20);
     }
 
-    const handleDClick = (name, e) => {
-        console.log('I am '+ name, e.target);
-    }
+    const handleDClick = () => {
+        setName('Suja');
+        setAge(81);
+     }
 
     return ( 
         <div className="home">
             <h1>Homepage</h1>
-            <button onClick={handleClick}>Click Me</button>
-            <button onDoubleClick= {(e) => handleDClick('sujju', e)}>Double Click Me</button>
+            <h3>{name} is {age} years old</h3>
+            <button onClick={handleClick} onDoubleClick = {handleDClick}>Click Me</button>
         </div>
      );
 }
