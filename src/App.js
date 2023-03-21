f2ab45ddaf9a9df5378b6c2ce092b5768a529c57
1sarthakbhardwaj <7sarthakbhardwaj@gmail.com>
2023-03-22 01:22:23 +0530

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   // Only number string are fine, not for boolean and objects
-  const title = 'Welcome to the new blog';
-  const likes = 50;
+
 
   return (
     <div className="App">
-      <div className="content">
-        <h1>{title}</h1>
-        <p>You got {likes} likes</p>
-        <p>random number is {Math.random() * 100}</p>
-      </div>
+      <Navbar />
+      <Home />
     </div>
   );
 }
