33ba1051e2ddd4bee02b7bb52714c105c2b2f866
1sarthakbhardwaj <7sarthakbhardwaj@gmail.com>
2023-03-22 01:58:12 +0530

    Click events

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('clicked');
+    }
+
+    const handleDClick = (name, e) => {
+        console.log('I am '+ name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h1>Homepage</h1>
+            <button onClick={handleClick}>Click Me</button>
+            <button onDoubleClick= {(e) => handleDClick('sujju', e)}>Double Click Me</button>
         </div>
      );
 }
