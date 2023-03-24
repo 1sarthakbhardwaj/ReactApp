8bf0562224881bdf7b487c588dfb8fcf0de1c845
1sarthakbhardwaj <7sarthakbhardwaj@gmail.com>
2023-03-24 10:47:45 +0530

    added forms

--- src/create.jsx
+++ src/create.jsx
@@ -1,18 +1,40 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, SetTitle] = useState ('hello');
+    const [body, SetBody] = useState ('hello');
+    const [author, SetAuthor] = useState ('hello');
+
     return ( 
         <div className="create">
             <h2>Create a new blog</h2>
             <form>
                 <label>Blog title:</label>
-                <input type="text" required />
+                <input 
+                type="text"
+                 required 
+                 value={title}
+                 onChange = {(e) => SetTitle(e.target.value)}
+                 />
                 <label>Blog body:</label>
-                <textarea required></textarea>
+                <textarea required
+                value={body}
+                onChange = {(e) => SetBody(e.target.value)}
+                ></textarea>
+                
                 <label>Blog author:</label>
-                <select>
+                <select
+                value={author}
+                onChange = {(e) => SetAuthor(e.target.value)}>
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
                 <button>Add blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+
+
                 </form>
         </div>
      );
