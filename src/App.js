5d46d5b178569a6a69169dad509951a0f7a6b26c
1sarthakbhardwaj <7sarthakbhardwaj@gmail.com>
2023-03-24 22:48:30 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Create from './create';
+import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
-  // Only number string are fine, not for boolean and objects
-
-
   return (
     <Router>
       <div className="App">
-      <Navbar />
-      <div className="content">
-      <Switch>
-        <Route exact path = "/">
-          <Home />  
-        </Route>
-        <Route exact path = "/create">
-          <Create/>  
-        </Route>
-        <Route exact path = "/blogs/:id">
-          <BlogDetails/>  
-        </Route>
-      </Switch>
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
     </Router>
   );
 }
--- src/create.jsx
+++ src/create.jsx
@@ -1,43 +1,55 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
-    const [title, SetTitle] = useState ('hello');
-    const [body, SetBody] = useState ('hello');
-    const [author, SetAuthor] = useState ('hello');
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const history = useHistory();
 
-    return ( 
-        <div className="create">
-            <h2>Create a new blog</h2>
-            <form>
-                <label>Blog title:</label>
-                <input 
-                type="text"
-                 required 
-                 value={title}
-                 onChange = {(e) => SetTitle(e.target.value)}
-                 />
-                <label>Blog body:</label>
-                <textarea required
-                value={body}
-                onChange = {(e) => SetBody(e.target.value)}
-                ></textarea>
-                
-                <label>Blog author:</label>
-                <select
-                value={author}
-                onChange = {(e) => SetAuthor(e.target.value)}>
-                    <option value="mario">mario</option>
-                    <option value="yoshi">yoshi</option>
-                </select>
-                <button>Add blog</button>
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
 
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      // history.go(-1);
+      history.push('/');
+    })
+  }
 
-                </form>
-        </div>
-     );
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
 }
  
 export default Create;
